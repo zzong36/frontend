5b0446e0e24eeac0e1f23ea604d6abda585610b6
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-05-13 14:13:46 +0900

    a

--- Day3/lab0301/poly.js
+++ Day3/lab0301/poly.js
@@ -6,3 +6,8 @@ const menu=document.querySelector(".navbar_menu")
 const icons=document.querySelector(".navbar_icons")
 
 // 이벤트를 사용할려면 객체를 만들고 이벤트 리스너를 붙여야 사용이 가능하다.
+
+togleBtn.addEventListener("click", ()=>{
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
