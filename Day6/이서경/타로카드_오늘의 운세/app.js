472e1a245500765b8bd12bd24dac535edf3bb631
seokyung <lastsun4@naver.com>
2022-05-23 02:37:10 +0900

    a

--- Day6/이서경/타로카드_오늘의 운세/app.js
+++ Day6/이서경/타로카드_오늘의 운세/app.js
@@ -5,7 +5,7 @@ const root = document.querySelector("#root");
 // 테이블에 이미지를 담아서 정렬
 function loadImage(){
     let arr = tarotCard();
-    let str = `<center><table id = "t" width ="700px"><tr><td>${arr[0]}</td><td>${arr[1]}</td><td>${arr[2]}</td></tr></table></center>`
+    let str = `<table width ="700px"><tr><td>${arr[0]}</td><td>${arr[1]}</td><td>${arr[2]}</td></tr><tr><td>Past</td><td>Present</td><td>Future</td></tr></table>`
     return str;
 };
 root.innerHTML=loadImage();
