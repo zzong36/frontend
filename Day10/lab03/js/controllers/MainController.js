9b4051ee3a282b821953cdfee202fb71e61b2838
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-05-24 15:58:23 +0900

    d

--- Day10/lab03/js/controllers/MainController.js
+++ Day10/lab03/js/controllers/MainController.js
@@ -7,16 +7,35 @@ const tag = "[MainController]";
 export default{
     init(){
         console.log(tag, "init()");
-        FormView.setup(document.querySelector('form')).on('@submit', e => this.onSubmit(e.detail.input)).on('@reset', e => this.resetForm());
+        FormView.setup(document.querySelector('form'))
+        .on('@submit', e => this.onSubmit(e.detail.input))
+        .on('@reset', e => this.resetForm());
+
+        // SearchResultView 셋업할 때 #search-result 엘리먼트를 셋팅해준다.
+        SearchResultView.setup(document.querySelector('#search-result'))
     }, 
+
+    // 검색이 실행될 메소드를 구현한다.
+    search(){
+        console.log(tag, "search()", query);
+    // 서치 결과를 처리할 메소드를 하나 더 호출해준다.
+    this.onSearchResult([]);
+    },
 
     onSubmit(input){
         console.log(tag, 'onSubmit()'+input());
+        // 검색을 하고 엔터를 쳤을 경우니까 onSubmit이 일어났을 때
+        // 검색하는 메소드가 호출되면 된다.
+        this.search(input); 
     },
 
     // reset 이벤트가 일어날 때 처리될 메소드
     resetForm(){
         console.log(tag, "onResetForm()");
+    },
+
+    onSearchResult(data){
+        SearchResultView
     }
 }
 
---
+++ Day10/lab03/js/views/SearchResultView.js
@@ -0,0 +1,16 @@
+SearchResultView.setup = function(element){
+    this.init(element);
+    this.searchRstE1 = element.querySelector("#search-result");
+    return this;
+}
+
+// 데이터가 없을 경우와 있을 경우에 대한 처리
+SearchResultView.render=function(data=[]){
+    console.log(tag, 'render', data);
+    this.searchRstE1.innerHTML=data.length?this.getSearchResultHtml(data): "검색결과가 없습니다.";
+}
+
+// 검색결과가 존재한다면, html tag가 들어가게 될 예정
+SearchResultView.getSearchResultHtml=function(data){
+    console.log(tag, 'getSearchResultHtml ', data)
+}
